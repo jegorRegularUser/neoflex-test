0c38443a263724c44117ff562d416a145e193677
Jegor <Dvoinyakovegor00@gmail.com>
2024-09-23 19:33:40 +0300

    Update Header.tsx

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -33,6 +33,7 @@ export default function Header() {
           <SvgIcon icon="favorite" />
           <span className="count">1</span>
         </Link>
+        <h4>abgiusdibuay</h4>
         <Link className="link" to="/cart">
           <SvgIcon icon="cart" />
           {cartItemsQuantity !== 0 && (
